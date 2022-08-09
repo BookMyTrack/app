ee3e954d58f24ded381bc0badc8625d4f1e8ba8b
Federico <fedevitale99@gmail.com>
2022-08-09 15:42:12 +0200

    responsive homescreen

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -22,7 +22,7 @@ export default function Index() {
   const tracks: track.Track[] = useLoaderData();
 
   return (
-    <div className="max-w-8xl justify-items-center grid items-start w-full h-full grid-cols-3 gap-4 p-8 mx-auto">
+    <div className="max-w-8xl justify-items-center grid items-start w-full h-full md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 p-8 mx-auto">
       {tracks && tracks.length === 0 && <p> No tracks available </p>}
       {tracks.map((track) => (
         <Link
